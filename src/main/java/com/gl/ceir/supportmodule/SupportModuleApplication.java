e772404248cebf193fc83ea61a43e19ae3a58c75
radhay-1199 <radhay.1199@gmail.com>
2024-11-27 04:25:35 +0530

    updated code 27/11

--- src/main/java/com/gl/ceir/supportmodule/SupportModuleApplication.java
+++ src/main/java/com/gl/ceir/supportmodule/SupportModuleApplication.java
@@ -2,10 +2,12 @@ package com.gl.ceir.supportmodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class SupportModuleApplication {
 
 	public static void main(String[] args) {
