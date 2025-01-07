d7ea186175b777c0c05af4e769b40d459674d536
kunalMathuriya <kunalmathuria27@gmail.com>
2025-01-07 18:22:00 +0530

    changes in DuplicateDeviceDetail model

--- src/main/java/com/gl/ceir/supportmodule/model/app/DuplicateDeviceDetail.java
+++ src/main/java/com/gl/ceir/supportmodule/model/app/DuplicateDeviceDetail.java
@@ -57,30 +57,6 @@ public class DuplicateDeviceDetail {
     @Column(name = "transaction_id")
     private String transactionId;
 
-    @Column(name = "document_type1")
-    private String documentType1;
-
-    @Column(name = "document_type2")
-    private String documentType2;
-
-    @Column(name = "document_type3")
-    private String documentType3;
-
-    @Column(name = "document_type4")
-    private String documentType4;
-
-    @Column(name = "document_file_name_1")
-    private String documentFileName1;
-
-    @Column(name = "document_file_name_2")
-    private String documentFileName2;
-
-    @Column(name = "document_file_name_3")
-    private String documentFileName3;
-
-    @Column(name = "document_file_name_4")
-    private String documentFileName4;
-
     @Column(name = "reminder_status")
     private Integer reminderStatus;
 
@@ -89,24 +65,6 @@ public class DuplicateDeviceDetail {
 
     @Column(name = "fail_count")
     private Integer failCount;
-
-    @Column(name = "approve_transaction_id")
-    private String approveTransactionId;
-
-    @Column(name = "approve_remark")
-    private String approveRemark;
-
-    @Column(name = "document_path1")
-    private String documentPath1;
-
-    @Column(name = "document_path2")
-    private String documentPath2;
-
-    @Column(name = "document_path3")
-    private String documentPath3;
-
-    @Column(name = "document_path4")
-    private String documentPath4;
 
     @Column(name = "actual_imei")
     private String actualImei;
